c0f8606017b75d97513f4844e47df578948c6382
Shreeram <shreeram.v@live.com>
2023-11-25 10:45:26 +0530

    feat: fixing few linting errors

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+"""
+Code to convert text translation from any language to any other language
+"""
+from tempfile import NamedTemporaryFile
+import time
 from googletrans import Translator
 from gtts import gTTS
 import pygame
-from tempfile import NamedTemporaryFile
-
+pygame.init() # Using pygame for playing the audio - converted speech
+INPUT_STRING="Hello, My name is Ram"
 translator = Translator()
-translated = translator.translate('veritas lux mea', src='la', dest='ta')
-
+translated = translator.translate(INPUT_STRING, dest='ta')
 def convert_tamil_to_speech(tamil_text):
+    '''
+    Convert tamil text to speech and saves the instance of mp3 file
+    '''
     tts = gTTS(text=tamil_text, lang='ta')
     tts_file = NamedTemporaryFile(suffix='.mp3')
     tts.save(tts_file.name)
     return tts_file
-
 tamil_translation = translated.text
 tts_file = convert_tamil_to_speech(tamil_translation)
-
-pygame.init()
-
 pygame.mixer.music.load(tts_file.name)
 pygame.mixer.music.play()
-
-input("Press enter to stop playback...")
-pygame.mixer.music.stop()
+while pygame.mixer.music.get_busy():  # wait for music to finish playing
+    time.sleep(1)
+# Lets user to stop by pressing enter
+# input("Press enter to stop playback...")
+# pygame.mixer.music.stop()
